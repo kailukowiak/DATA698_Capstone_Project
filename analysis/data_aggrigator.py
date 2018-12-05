1a207b4571705d5e560d3884c59344c376efe4eb
Kai <kailukowiak@gmail.com>
2018-12-04 21:57:06 -0700

    demeaned Calgary

--- analysis/data_aggrigator.py
+++ analysis/data_aggrigator.py
@@ -3,6 +3,8 @@ import pandas as pd
 from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
 from scipy.spatial.distance import squareform
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 prices = pd.read_pickle("scraper_app/prices.pkl")
 
@@ -35,7 +37,7 @@ df['margin'] = df.price/(1+gst)
 ab_cities = ['calgary', 'edmonton', 'lethbridge', 'red deer']
 
 df.margin.loc[df.area_name.isin(ab_cities)] = \
-    df.margin.loc[df.area_name.isin(ab_cities)] -alberta_excise
+    df.margin.loc[df.area_name.isin(ab_cities)] - alberta_excise
 df.margin.loc[df.area_name.isin(['vancouver'])] = \
     df.margin.loc[df.area_name.isin(['vancouver'])] - vancouver_excise
 df.margin.loc[df.area_name.isin(['kelowna'])] = \
@@ -45,8 +47,8 @@ df.margin.loc[df.area_name.isin(['victoria'])] = \
 
 
 margins = pd.read_csv("scraper_app/city_margins.csv")
-margins = margins.melt(id_vars='date', 
-                       value_name='rack', 
+margins = margins.melt(id_vars='date',
+                       value_name='rack',
                        var_name='area_name')
 
 margins.area_name = margins.area_name.str.lower()
@@ -57,7 +59,7 @@ margins.date = pd.to_datetime(margins.date)
 
 df = pd.merge(df, margins, on=['date', 'area_name'])
 df['margin'] = df.margin - df.rack
-## Test
+# Test
 
 df = df[['names', 'price', 'address', 'area_name',
          'lat', 'lng', 'time', 'margin']]
@@ -66,7 +68,7 @@ df = df[['names', 'price', 'address', 'area_name',
 df.time = df.time.dt.round("H")
 df.drop_duplicates(subset=['time', 'address'], inplace=True)
 
-## Test
+
 df_p = df.reset_index().pivot('time', 'address', 'margin')
 
 df_p = df_p.fillna(method='ffill', limit=72)
@@ -75,61 +77,55 @@ tmp = df_p.dropna(axis=1)
 
 cor_mat = tmp.corr()
 
-
-## Test
+#
 cor_mat.dropna(inplace=True)
 
-Z=linkage(cor_mat, 'single', 'correlation')
+# Z=linkage(cor_mat, 'single', 'correlation')
 
-dendrogram(Z, color_threshold=0);
+# dendrogram(Z, color_threshold=0);
 
 missing = df_p.isna().sum().sort_values()
 
-
-
-
 calg = df.loc[df['area_name'] == "calgary"]
-calg = calg.pivot('time', 'address', 'margin')
+calg.set_index('time', inplace=True)
+calg = calg.sort_index()
+# %%
+calg['rolling_mean'] = calg.margin.rolling("1d", min_periods=1).mean()
+calg.margin = calg.margin - calg.rolling_mean
+
+
+# %%
+
+
+calg = calg.pivot(None, 'address', 'margin')
 calg = calg.fillna(method='ffill', limit=72)
 calg = calg.fillna(method='bfill', limit=72)
 calg_missing = calg.isna().sum().sort_values()
 calg_missing = calg_missing.loc[calg_missing > 1]
 
-calg.drop((calg_missing.index), axis = 1, inplace=True)
+calg.drop((calg_missing.index), axis=1, inplace=True)
 
 # calg.roll
 
 calg_cor = calg.corr()
 calg_cor.isna().sum()
 
-
-
-
-#
-
 dissimilarity = 1 - np.abs(calg_cor)
 hierarchy = linkage(squareform(dissimilarity), method='average')
-labels = fcluster(hierarchy, 0.38, criterion='distance')
+labels = fcluster(hierarchy, 0.8, criterion='distance')
 labels
-Z=linkage(dissimilarity, 'single', 'correlation')
+Z = linkage(dissimilarity, 'single', 'correlation')
 
-dendrogram(Z, color_threshold=0.15);
+dendrogram(Z, color_threshold=0.15)
 
-calg_clusters = pd.DataFrame({'cluster': labels}, index =calg_cor.index)
+calg_clusters = pd.DataFrame({'cluster': labels}, index=calg_cor.index)
 calg = df.loc[df['area_name'] == "calgary"]
-calg = calg.set_index(keys = ['address', 'time'])
+calg = calg.set_index(keys=['address', 'time'])
 
 calg = calg.join(calg_clusters, how='inner')
-#calg.cluster = str(calg.cluster)
+# calg.cluster = str(calg.cluster)
 
-
-import matplotlib.pyplot as plt
-import seaborn as sns
-letters = ['a', 'b', 'c', 'd'] # , 'e', 'f', 'g', 'h'
-calg.cluster = calg.cluster.replace([1, 2, 3, 4], letters)
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']  # , 'e', 'f', 'g', 'h'
+calg.cluster = calg.cluster.replace([1, 2, 3, 4, 5, 6, 7], letters)
 # calg['cluster_cat'] = calg.cluster
-sns.scatterplot('lat', 'lng', hue='cluster', data=calg, )
-
-
-with sns.color_palette("husl", 8):
-    _ = sns.scatterplot('lat', 'lng', hue='cluster', data=calg, legend=False)
+sns.scatterplot('lat', 'lng', hue='cluster', data=calg)
